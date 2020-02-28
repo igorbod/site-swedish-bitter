4bd5332ac024d7aafff07a51567f216f803d96e4
Igor <igorbod.webdev@gmail.com>
2020-02-28 14:37:19 +0700

    add hover effect to each item in section Composition, using JavaScript

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+'use strict';
+
+let compositionItem = document.querySelectorAll('.composition__item'),
+    compositionItemHover = document.querySelectorAll('.composition__item-hover');
+
+// for section Composition
+compositionItem.forEach(function(item, i) {
+  item.addEventListener('mouseenter', function () {
+    compositionItemHover[i].style.opacity = ".80";
+    item.style.cursor = "pointer";
+  });
+
+  item.addEventListener('mouseleave', function () {
+    compositionItemHover[i].style.opacity = "0";
+  });
+});
+
+
 $(function() {
   $('.symptoms__tabs-wrapper .tab').on('click', function (event) {
     let id = $(this).attr('data-id');
@@ -8,3 +26,4 @@ $(function() {
     return false;
   });
 });
+
